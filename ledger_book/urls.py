2a0db3d2722316c655e3831b4cbfe4b0db311cc8
Tarique Anwer <itzmetariq@gmail.com>
2021-02-01 14:20:03 +0530

    implemented: add listing API endpoints for Source & Person #6

--- ledger_book/urls.py
+++ ledger_book/urls.py
@@ -4,7 +4,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 app_name = 'ledger_book'
 urlpatterns = [
-    path('index/', views.index, name='index'),
+    path('source/', views.SourceView.as_view(), name='source'),
+    path('person/', views.PersonView.as_view(), name='person'),
     path('tx/', views.TransactionView.as_view(), name='tx'),
     path('balance/', views.BalanceView.as_view(), name='balance'),
 ]
